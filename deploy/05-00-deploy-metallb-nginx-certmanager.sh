9959aaca82057364db387800cef31d45720a322c
bpafoshizle <bpafoshizle@gmail.com>
2023-05-16 22:15:04 -0500

    added ingressClassName to media.ingress.yml

--- deploy/05-00-deploy-metallb-nginx-certmanager.sh
+++ deploy/05-00-deploy-metallb-nginx-certmanager.sh
@@ -1,2 +1,3 @@
 source ../kube/metallb-nginx-certmanager/00-install-metallb.sh
 source ../kube/metallb-nginx-certmanager/01-install-nginx.sh
+source ../kube/metallb-nginx-certmanager/02-install-certmanager.sh
