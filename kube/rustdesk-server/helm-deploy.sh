d35e39eb0685599583b0625ba289c8c0482110a7
bpafoshizle <bpafoshizle@gmail.com>
2024-02-04 21:09:19 -0600

    corrected PVC name

--- kube/rustdesk-server/helm-deploy.sh
+++ kube/rustdesk-server/helm-deploy.sh
@@ -2,4 +2,4 @@ helm repo add home-kube-setup https://bpafoshizle.github.io/home-kube-setup
 helm repo update
 
 helm install rustdesk-server home-kube-setup/rustdesk-server \
-    --values charts/rustdesk-server/values.yaml
+    --values ../kube/rustdesk-server/charts/rustdesk-server/values.yaml
