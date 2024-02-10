02c1abde419d863379de79fe1e2321f7c7d51a0f
bpafoshizle <bpafoshizle@gmail.com>
2024-02-10 16:21:41 -0600

    rejiggering rustdesk chart

--- kube/rustdesk-server/helm-deploy.sh
+++ kube/rustdesk-server/helm-deploy.sh
@@ -2,4 +2,4 @@ helm repo add home-kube-setup https://bpafoshizle.github.io/home-kube-setup
 helm repo update
 
 helm install rustdesk-server home-kube-setup/rustdesk-server \
-    --values ../kube/rustdesk-server/charts/rustdesk-server/values.yaml
+    --values ../rustdesk-server/charts/rustdesk-server/values.yaml
