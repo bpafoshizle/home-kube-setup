e28747f574dfc20821c3ab15270af22e85353a5e
bpafoshizle <bpafoshizle@gmail.com>
2024-07-22 13:14:31 -0400

    upgraded to latest helm-deployable version of kubernetes dashboard. Updated readme and deployment scripts to reflect

---
+++ kube/kubernetes-dashboard/01-install-adminuser.sh
@@ -0,0 +1,3 @@
+kubectl apply -f ../kube/kubernetes-dashboard/dashboard-adminuser-serviceaccount.yaml
+kubectl apply -f ../kube/kubernetes-dashboard/dashboard-adminuser-clusterrolebinding.yaml
+kubectl apply -f ../kube/kubernetes-dashboard/dashboard-adminuser-secret.yaml
